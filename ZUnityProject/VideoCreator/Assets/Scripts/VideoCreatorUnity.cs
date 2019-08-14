0c74eba21af1576f09155322be9bed5f4a35154f
fuziki <hymne.shannon.projects@gmail.com>
2019-08-15 00:22:09 +0900

    code VideoCreatorUnity.cs

--- ZUnityProject/VideoCreator/Assets/Scripts/VideoCreatorUnity.cs
+++ ZUnityProject/VideoCreator/Assets/Scripts/VideoCreatorUnity.cs
@@ -1,16 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class VideoCreatorUnity {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [DllImport("__Internal")]
+    private static extern IntPtr videoCreator_init(string tmpFilePath, bool enableAudio, int videoWidth, int videoHeight);
+
+    [DllImport("__Internal")]
+    private static extern bool videoCreator_isRecording(IntPtr creator);
+
+    [DllImport("__Internal")]
+    private static extern void videoCreator_startRecording(IntPtr creator);
+
+    [DllImport("__Internal")]
+    private static extern void videoCreator_append(IntPtr creator, IntPtr mtlTexture);
+
+    [DllImport("__Internal")]
+    private static extern void videoCreator_finishRecording(IntPtr creator);
+
+    private IntPtr creatorObject;
+    public VideoCreatorUnity(string tmpFilePath, bool enableAudio, int videoWidth, int videoHeight)
+    {
+        creatorObject = videoCreator_init(tmpFilePath, enableAudio, videoWidth, videoHeight);
+    }
+
+    public bool isRecording
+    {
+        get
+        {
+            return videoCreator_isRecording(creatorObject);
+        }
+    }
+
+    public void startRecording()
+    {
+        videoCreator_startRecording(creatorObject);
+    }
+
+    public void append(Texture texture)
+    {
+        videoCreator_append(creatorObject, texture.GetNativeTexturePtr());
+    }
+
+    public void finishRecording()
+    {
+        videoCreator_finishRecording(creatorObject);
+    }
 }
