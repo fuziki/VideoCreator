4a4895c0b04282f0389f2de00ac4e2582e6b6ad2
fuziki <hymne.shannon.projects@gmail.com>
2019-08-16 00:04:58 +0900

    add plugin

---
+++ ZUnityProject/VideoCreator/Assets/Plugin/iOS/VideoCreator.framework/Headers/VideoCreator.h
@@ -0,0 +1,19 @@
+//
+//  VideoCreator.h
+//  VideoCreator
+//
+//  Created by fuziki on 2019/08/06.
+//  Copyright © 2019 fuziki.factory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VideoCreator.
+FOUNDATION_EXPORT double VideoCreatorVersionNumber;
+
+//! Project version string for VideoCreator.
+FOUNDATION_EXPORT const unsigned char VideoCreatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VideoCreator/PublicHeader.h>
+
+
