2f5091f17c3a2530fc66e0efb0b33b71c42519ec
fuziki <hymne.shannon.projects@gmail.com>
2019-08-18 00:17:00 +0900

    change directory

---
+++ ZUnityProject/VideoCreator/Assets/Plugin/VideoCreator/Scripts/IVideoCreatorUnity.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace VideoCreator {
+    public interface IVideoCreatorUnity
+    {
+        bool IsRecording
+        {
+            get;
+        }
+        void StartRecording();
+        void Append(Texture texture);
+        void FinishRecording();
+    }
+}
