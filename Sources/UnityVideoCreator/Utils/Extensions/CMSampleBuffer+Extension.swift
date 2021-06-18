c092ad68de6c2d13189cc6874b8b761c60179059
fuziki <hymne.shannon.projects@gmail.com>
2021-06-19 01:17:58 +0900

    rename path

--- Sources/UnityVideoCreator/Utils/Extensions/CMSampleBuffer+Extension.swift
+++ Sources/UnityVideoCreator/Utils/Extensions/CMSampleBuffer+Extension.swift
@@ -69,3 +69,32 @@ extension CMSampleBuffer {
         }
     }
 }
+
+class CMSampleBufferAudioFactory {
+    func make(pcmBuffer: AVAudioPCMBuffer, time: CMTime) -> CMSampleBuffer? {
+        let audioBufferList: AudioBufferList = pcmBuffer.audioBufferList.pointee
+        var blockBuffer: CMBlockBuffer?
+        _ = CMBlockBufferCreateWithMemoryBlock(allocator: kCFAllocatorDefault,
+                                               memoryBlock: audioBufferList.mBuffers.mData,
+                                               blockLength: Int(audioBufferList.mBuffers.mDataByteSize),
+                                               blockAllocator: kCFAllocatorNull,
+                                               customBlockSource: nil,
+                                               offsetToData: 0,
+                                               dataLength: Int(audioBufferList.mBuffers.mDataByteSize),
+                                               flags: 0,
+                                               blockBufferOut: &blockBuffer)
+        let formatDescription: CMAudioFormatDescription = pcmBuffer.format.formatDescription
+        var sampleBuffer: CMSampleBuffer?
+        _ = CMAudioSampleBufferCreateWithPacketDescriptions(allocator: kCFAllocatorDefault,
+                                                            dataBuffer: blockBuffer,
+                                                            dataReady: true,
+                                                            makeDataReadyCallback: nil,
+                                                            refcon: nil,
+                                                            formatDescription: formatDescription,
+                                                            sampleCount: CMItemCount(pcmBuffer.frameLength),
+                                                            presentationTimeStamp: time,
+                                                            packetDescriptions: nil,
+                                                            sampleBufferOut: &sampleBuffer)
+        return sampleBuffer
+    }
+}
