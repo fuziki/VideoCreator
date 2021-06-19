e6484a7a4ee4fcb2b8545383a2016e505e6f0d64
fuziki <hymne.shannon.projects@gmail.com>
2021-06-19 21:00:42 +0900

    add WavExampleApp

---
+++ Examples/NativeExamples/WavExample/WavExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WavExampleApp.swift
+//  WavExample
+//
+//  Created by fuziki on 2021/06/19.
+//
+
+import SwiftUI
+
+@main
+struct WavExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
