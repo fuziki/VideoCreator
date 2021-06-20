4a084e5aec2c324168f1cedec07038add8583d11
fuziki <hymne.shannon.projects@gmail.com>
2021-06-20 20:18:06 +0900

    update unity example

--- Examples/UnityExample/Assets/Plugin/VideoCreator/Scripts/MediaCreator/MediaCreator.cs
+++ Examples/UnityExample/Assets/Plugin/VideoCreator/Scripts/MediaCreator/MediaCreator.cs
@@ -1,11 +1,22 @@
-//using System.Collections;
-//using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.Runtime.InteropServices;
 
 namespace VideoCreator
 {
+    public class MediaSaver
+    {
+#if UNITY_IOS
+        [DllImport("__Internal")]
+        private static extern void UnityMediaSaver_saveVideo(string url);
+
+        public static void SaveVideo(string url)
+        {
+            UnityMediaSaver_saveVideo(url);
+        }
+#endif
+    }
+
     public class MediaCreator
     {
 #if UNITY_IOS
@@ -34,42 +45,42 @@ namespace VideoCreator
         private static extern void UnityMediaCreator_writeAudio(float[] pcm, long frame, long microSec);
 #endif
 
-        public static void initAsMovWithNoAudio(string url, string codec, long width, long height)
+        public static void InitAsMovWithNoAudio(string url, string codec, long width, long height)
         {
 #if UNITY_IOS
             UnityMediaCreator_initAsMovWithNoAudio(url, codec, width, height);
 #endif
         }
 
-        public static void initAsMovWithAudio(string url, string codec, long width, long height, long channel, float samplingRate)
+        public static void InitAsMovWithAudio(string url, string codec, long width, long height, long channel, float samplingRate)
         {
 #if UNITY_IOS
             UnityMediaCreator_initAsMovWithAudio(url, codec, width, height, channel, samplingRate);
 #endif
         }
 
-        public static void initAsWav(string url, long channel, float samplingRate, long bitDepth)
+        public static void InitAsWav(string url, long channel, float samplingRate, long bitDepth)
         {
 #if UNITY_IOS
             UnityMediaCreator_initAsWav(url, channel, samplingRate, bitDepth);
 #endif
         }
 
-        public static void start(long microSec)
+        public static void Start(long microSec)
         {
 #if UNITY_IOS
             UnityMediaCreator_start(microSec);
 #endif
         }
 
-        public static void finishSync()
+        public static void FinishSync()
         {
 #if UNITY_IOS
             UnityMediaCreator_finishSync();
 #endif
         }
 
-        public static bool isRecording()
+        public static bool IsRecording()
         {
 #if UNITY_IOS
             return UnityMediaCreator_isRecording();
@@ -78,14 +89,14 @@ namespace VideoCreator
 #endif
         }
 
-        public static void writeVideo(Texture texture, long microSec)
+        public static void WriteVideo(Texture texture, long microSec)
         {
 #if UNITY_IOS
             UnityMediaCreator_writeVideo(texture.GetNativeTexturePtr(), microSec);
 #endif
         }
 
-        public static void writeAudio(float[] pcm, long microSec)
+        public static void WriteAudio(float[] pcm, long microSec)
         {
 #if UNITY_IOS
             UnityMediaCreator_writeAudio(pcm, pcm.Length, microSec);
---
+++ Examples/UnityExample/Assets/Scripts/RecordingController.cs
@@ -0,0 +1,166 @@
+using UnityEngine;
+using UnityEngine.UI;
+using VideoCreator;
+
+[RequireComponent(typeof(AudioSource))]
+public class RecordingController : MonoBehaviour
+{
+    public RenderTexture texture = null;
+
+    public Text text;
+
+    private bool isRecording = false;
+    private bool recordTexture = false;
+    private bool recordAudio = false;
+    private bool saveAfterFinish = false;
+
+    private string cachePath = "";
+
+    private long amountFrame = 0;
+    private float startTime = 0;
+
+    private long startTimeOffset = 6_000_000;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 30;
+
+        var source = gameObject.AddComponent<AudioSource>();
+        source.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!recordTexture) return;
+        if (!MediaCreator.IsRecording()) return;
+
+        long time = (long)((Time.time - startTime) * 1_000_000) + startTimeOffset;
+
+        Debug.Log($"write texture: {time}");
+
+        MediaCreator.WriteVideo(texture, time);
+    }
+
+    public void StartRecMovWithAudio()
+    {
+        if (isRecording) return;
+        text.text = "start rec mov with audio!";
+
+        var source = gameObject.AddComponent<AudioSource>();
+        var clip = Microphone.Start(null, true, 1, 48_000);
+        source.clip = clip;
+        source.loop = true;
+        while (Microphone.GetPosition(null) < 0) { }
+
+        cachePath = "file://" + Application.temporaryCachePath + "/tmp.mov";
+        Debug.Log($"cachePath: {cachePath}, {texture.width}x{texture.height}");
+
+        MediaCreator.InitAsMovWithAudio(cachePath, "h264", texture.width, texture.height, 1, 48_000);
+        MediaCreator.Start(startTimeOffset);
+
+        startTime = Time.time;
+
+        isRecording = true;
+        recordTexture = true;
+        recordAudio = true;
+        saveAfterFinish = true;
+        amountFrame = 0;
+
+        source.Play();
+    }
+
+    public void StartRecMovWithNoAudio()
+    {
+        if (isRecording) return;
+        text.text = "start rec mov with no audio!";
+
+        cachePath = "file://" + Application.temporaryCachePath + "/tmp.mov";
+        Debug.Log($"cachePath: {cachePath}, {texture.width}x{texture.height}");
+
+        MediaCreator.InitAsMovWithNoAudio(cachePath, "h264", texture.width, texture.height);
+        MediaCreator.Start(startTimeOffset);
+
+        startTime = Time.time;
+
+        isRecording = true;
+        recordTexture = true;
+        recordAudio = false;
+        saveAfterFinish = true;
+        amountFrame = 0;
+    }
+
+    public void StartRecWav()
+    {
+        if (isRecording) return;
+        text.text = "start rec wav!";
+
+        var source = gameObject.AddComponent<AudioSource>();
+        var clip = Microphone.Start(null, true, 1, 48_000);
+        source.clip = clip;
+        source.loop = true;
+        while (Microphone.GetPosition(null) < 0) { }
+
+        cachePath = "file://" + Application.temporaryCachePath + "/tmp.wav";
+        Debug.Log($"cachePath: {cachePath}");
+
+        MediaCreator.InitAsWav(cachePath, 1, 48000, 32);
+        MediaCreator.Start(startTimeOffset);
+
+        startTime = Time.time;
+
+        isRecording = true;
+        recordTexture = false;
+        recordAudio = true;
+        saveAfterFinish = false;
+        amountFrame = 0;
+
+        source.Play();
+    }
+
+    public void FinishRec()
+    {
+        if (!isRecording) return;
+        text.text = "finish recording";
+
+        var source = gameObject.AddComponent<AudioSource>();
+        source.Stop();
+
+        MediaCreator.FinishSync();
+        if (saveAfterFinish)
+        {
+            MediaSaver.SaveVideo(cachePath);
+        }
+
+        isRecording = false;
+        recordTexture = false;
+        recordAudio = false;
+        saveAfterFinish = false;
+    }
+
+    void OnAudioFilterRead(float[] data, int channels)
+    {
+        writeAudio(data, channels);
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            data[i] = 0;
+        }
+    }
+
+    private void writeAudio(float[] data, int channels)
+    {
+        if (!recordAudio) return;
+        if (!MediaCreator.IsRecording()) return;
+
+
+        long time = (amountFrame * 1_000_000 / 48_000) + startTimeOffset;
+        Debug.Log($"write audio: {time}");
+
+        MediaCreator.WriteAudio(data, time);
+
+        amountFrame += data.Length;
+    }
+}
