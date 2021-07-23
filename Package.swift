33b86a55f29d4b7f7ed7d173f125563e17cb9350
fuziki <hymne.shannon.projects@gmail.com>
2021-07-23 21:43:45 +0900

    add mac support for test

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "UnityVideoCreator",
     platforms: [
         .iOS(.v13),
+        .macOS(.v10_15),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
