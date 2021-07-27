b4ac0d5dbce1327aaeb9aac53a085d74e0277035
fuziki <hymne.shannon.projects@gmail.com>
2021-07-28 02:44:08 +0900

    refactor native

---
+++ Sources/UnityVideoCreator/Utils/Unmanaged/Bridge.swift
@@ -0,0 +1,13 @@
+//
+//  Bridge.swift
+//  
+//
+//  Created by fuziki on 2021/07/28.
+//
+
+import Foundation
+
+// UnsafeRawPointer == UnsafePointer<Void> == (void*)
+func __bridge<T : AnyObject>(_ ptr: UnsafeRawPointer) -> T {
+    return Unmanaged.fromOpaque(ptr).takeUnretainedValue()
+}
