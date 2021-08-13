0a90af711f9e74ac9a487d8b2ef2f9cad47731b1
fuziki <hymne.shannon.projects@gmail.com>
2021-08-14 00:44:37 +0900

    available macOS11

--- Sources/UnityVideoCreator/Utils/Extensions/AVAssetSegmentTrackReport+Extensions.swift
+++ Sources/UnityVideoCreator/Utils/Extensions/AVAssetSegmentTrackReport+Extensions.swift
@@ -8,14 +8,14 @@
 import AVFoundation
 import Foundation
 
-@available(iOS 14.0, *)
+@available(iOS 14.0, macOS 11.0, *)
 extension AVAssetSegmentTrackReport {
     var debugMessage: String {
         return "\(description) trackID: \(trackID), mediaType: \(mediaType), earliestPresentationTimeStamp: \(earliestPresentationTimeStamp.seconds), duration: \(duration.seconds), firstVideoSampleInformation: \( firstVideoSampleInformation?.debugMessage ?? "nil")"
     }
 }
 
-@available(iOS 14.0, *)
+@available(iOS 14.0, macOS 11.0, *)
 extension AVAssetSegmentReportSampleInformation {
     var debugMessage: String {
         return "\(description) presentationTimeStamp: \(presentationTimeStamp.seconds), offset: \(offset), length: \(length), isSyncSample \(isSyncSample)"
