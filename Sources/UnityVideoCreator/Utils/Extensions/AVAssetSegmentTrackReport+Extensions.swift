53b57545d18f599a880dc820cfff3df2fc9d0763
fuziki <hymne.shannon.projects@gmail.com>
2021-08-14 14:58:07 +0900

    add str

--- Sources/UnityVideoCreator/Utils/Extensions/AVAssetSegmentTrackReport+Extensions.swift
+++ Sources/UnityVideoCreator/Utils/Extensions/AVAssetSegmentTrackReport+Extensions.swift
@@ -11,16 +11,16 @@ import Foundation
 @available(iOS 14.0, macOS 11.0, *)
 extension AVAssetSegmentTrackReport {
     var debugMessage: String {
-        let earliestPresentationTimeStamp = String(format: "%.3f", earliestPresentationTimeStamp.seconds)
-        let duration = String(format: "%.3f", duration.seconds)
-        return "\(description) trackID: \(trackID), mediaType: \(mediaType), earliestPresentationTimeStamp: \(earliestPresentationTimeStamp), duration: \(duration), firstVideoSampleInformation: \( firstVideoSampleInformation?.debugMessage ?? "nil")"
+        let earliestPresentationTimeStampStr = String(format: "%.3f", earliestPresentationTimeStamp.seconds)
+        let durationStr = String(format: "%.3f", duration.seconds)
+        return "\(description) trackID: \(trackID), mediaType: \(mediaType), earliestPresentationTimeStamp: \(earliestPresentationTimeStampStr), duration: \(durationStr), firstVideoSampleInformation: \( firstVideoSampleInformation?.debugMessage ?? "nil")"
     }
 }
 
 @available(iOS 14.0, macOS 11.0, *)
 extension AVAssetSegmentReportSampleInformation {
     var debugMessage: String {
-        let presentationTimeStamp = String(format: "%.3f", presentationTimeStamp.seconds)
-        return "\(description) presentationTimeStamp: \(presentationTimeStamp), offset: \(offset), length: \(length), isSyncSample \(isSyncSample)"
+        let presentationTimeStampStr = String(format: "%.3f", presentationTimeStamp.seconds)
+        return "\(description) presentationTimeStamp: \(presentationTimeStampStr), offset: \(offset), length: \(length), isSyncSample \(isSyncSample)"
     }
 }
