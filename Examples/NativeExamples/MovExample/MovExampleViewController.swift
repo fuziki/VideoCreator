4798d201643e710cf73c068d990e69b8d185a2d2
fuziki <hymne.shannon.projects@gmail.com>
2021-08-15 21:21:58 +0900

    add MovExampleViewController

---
+++ Examples/NativeExamples/MovExample/MovExampleViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MovExampleViewController.swift
+//  MovExample
+//
+//  Created by fuziki on 2021/08/15.
+//
+
+import UIKit
+
+class MovExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
