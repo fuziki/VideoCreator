22478ed2efeac409153c2219c3e022c83a867374
fuziki <hymne.shannon.projects@gmail.com>
2021-12-05 16:38:03 +0900

    add pip example

---
+++ Examples/NativeExamples/PipExamle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PipExamle
+//
+//  Created by fuziki on 2021/12/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
