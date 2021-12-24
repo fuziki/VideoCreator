1f7ccbfb9ece319355756f9f6482f2600bc29394
fuziki <hymne.shannon.projects@gmail.com>
2021-12-25 01:12:19 +0900

    add Shared SurveyH264App

---
+++ Examples/NativeExamples/Shared/SurveyH264App.swift
@@ -0,0 +1,17 @@
+//
+//  SurveyH264App.swift
+//  Shared
+//
+//  Created by fuziki on 2021/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SurveyH264App: App {
+    var body: some Scene {
+        WindowGroup {
+            H264ContentView()
+        }
+    }
+}
