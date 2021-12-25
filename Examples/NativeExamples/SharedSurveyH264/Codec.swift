ebe4367a3967047a529fb8a507571136bc1561e0
fuziki <hymne.shannon.projects@gmail.com>
2021-12-26 04:08:37 +0900

    wip decoder

--- Examples/NativeExamples/SharedSurveyH264/Codec.swift
+++ Examples/NativeExamples/SharedSurveyH264/Codec.swift
@@ -13,32 +13,35 @@ import VideoToolbox
 
 class Encoder {
     var session: VTCompressionSession?
-    
+
     let encodedSampleBuffer: AnyPublisher<CMSampleBuffer, Never>
     private let encodedSampleBufferSubject = PassthroughSubject<CMSampleBuffer, Never>()
 
     init() {
         encodedSampleBuffer = encodedSampleBufferSubject.eraseToAnyPublisher()
     }
-    
+
     func setup(width: Int32, height: Int32) {
         var session: VTCompressionSession?
         let encoderSpecification: CFDictionary? = nil
         let imageBufferAttributes: CFDictionary? = nil
-        VTCompressionSessionCreate(allocator: kCFAllocatorDefault,
-                                   width: width,
-                                   height: height,
-                                   codecType: kCMVideoCodecType_H264,
-                                   encoderSpecification: encoderSpecification,
-                                   imageBufferAttributes: imageBufferAttributes,
-                                   compressedDataAllocator: nil,
-                                   outputCallback: outputCallback,
-                                   refcon: Unmanaged.passUnretained(self).toOpaque(),
-                                   compressionSessionOut: &session)
+        let res = VTCompressionSessionCreate(allocator: kCFAllocatorDefault,
+                                             width: width,
+                                             height: height,
+                                             codecType: kCMVideoCodecType_H264,
+                                             encoderSpecification: encoderSpecification,
+                                             imageBufferAttributes: imageBufferAttributes,
+                                             compressedDataAllocator: nil,
+                                             outputCallback: outputCallback,
+                                             refcon: Unmanaged.passUnretained(self).toOpaque(),
+                                             compressionSessionOut: &session)
+        if res != noErr {
+            print("failed create VTCompressionSession")
+            return
+        }
         self.session = session
     }
-    
-    
+
     let outputCallback: VTCompressionOutputCallback = { (outputCallbackRefCon: UnsafeMutableRawPointer?,
                                                          sourceFrameRefCon: UnsafeMutableRawPointer?,
                                                          status: OSStatus,
@@ -52,7 +55,7 @@ class Encoder {
                         infoFlags: infoFlags,
                         sampleBuffer: sampleBuffer)
     }
-    
+
     func callback(outputCallbackRefCon: UnsafeMutableRawPointer?,
                   sourceFrameRefCon: UnsafeMutableRawPointer?,
                   status: OSStatus,
@@ -69,33 +72,135 @@ class Encoder {
         }
         encodedSampleBufferSubject.send(sampleBuffer)
     }
-    
+
     func encode(imageBuffer: CVImageBuffer, presentationTimeStamp: CMTime, duration: CMTime) {
-        var flags: VTEncodeInfoFlags = []
         if session == nil {
             setup(width: Int32(CVPixelBufferGetWidth(imageBuffer)), height: Int32(CVPixelBufferGetHeight(imageBuffer)))
         }
         guard let session = session else { return }
         print("encode frame")
-        VTCompressionSessionEncodeFrame(session,
-                                        imageBuffer: imageBuffer,
-                                        presentationTimeStamp: presentationTimeStamp,
-                                        duration: duration,
-                                        frameProperties: nil,
-                                        sourceFrameRefcon: nil,
-                                        infoFlagsOut: &flags)
+        var infoFlagsOut: VTEncodeInfoFlags = []
+        let res = VTCompressionSessionEncodeFrame(session,
+                                                  imageBuffer: imageBuffer,
+                                                  presentationTimeStamp: presentationTimeStamp,
+                                                  duration: duration,
+                                                  frameProperties: nil,
+                                                  sourceFrameRefcon: nil,
+                                                  infoFlagsOut: &infoFlagsOut)
+        if res != noErr {
+            print("faield encode frame: \(res)")
+        }
     }
 }
 
 class Decoder {
+    var session: VTDecompressionSession?
+
     let decoded: AnyPublisher<CMSampleBuffer, Never>
     private let decodedSubject = PassthroughSubject<CMSampleBuffer, Never>()
 
     init() {
         decoded = decodedSubject.eraseToAnyPublisher()
     }
-    
+
+    func setup() {
+        var session: VTDecompressionSession?
+        // TODO: 何とかする
+        let formatDescription: CMFormatDescription! = nil
+        let decoderSpecification: CFDictionary? = nil
+        let imageBufferAttributes: CFDictionary? = nil
+        var outputCallback = VTDecompressionOutputCallbackRecord(decompressionOutputCallback: decompressionOutputCallback,
+                                                                 decompressionOutputRefCon: Unmanaged.passUnretained(self).toOpaque())
+        let res = VTDecompressionSessionCreate(allocator: kCFAllocatorDefault,
+                                               formatDescription: formatDescription,
+                                               decoderSpecification: decoderSpecification,
+                                               imageBufferAttributes: imageBufferAttributes,
+                                               outputCallback: &outputCallback,
+                                               decompressionSessionOut: &session)
+        if res != noErr {
+            print("failed create VTDecompressionSession")
+            return
+        }
+        self.session = session
+    }
+
+    var decompressionOutputCallback: VTDecompressionOutputCallback = { (decompressionOutputRefCon: UnsafeMutableRawPointer?,
+                                                                        sourceFrameRefCon: UnsafeMutableRawPointer?,
+                                                                        status: OSStatus,
+                                                                        infoFlags: VTDecodeInfoFlags,
+                                                                        imageBuffer: CVImageBuffer?,
+                                                                        presentationTimeStamp: CMTime,
+                                                                        presentationDuration: CMTime) in
+        guard let decompressionOutputRefCon = decompressionOutputRefCon else { return }
+        let refcon = Unmanaged<Decoder>.fromOpaque(decompressionOutputRefCon).takeUnretainedValue()
+        refcon.callback(decompressionOutputRefCon: decompressionOutputRefCon,
+                        sourceFrameRefCon: sourceFrameRefCon,
+                        status: status,
+                        infoFlags: infoFlags,
+                        imageBuffer: imageBuffer,
+                        presentationTimeStamp: presentationTimeStamp,
+                        presentationDuration: presentationDuration)
+    }
+
+    func callback(decompressionOutputRefCon: UnsafeMutableRawPointer?,
+                  sourceFrameRefCon: UnsafeMutableRawPointer?,
+                  status: OSStatus,
+                  infoFlags: VTDecodeInfoFlags,
+                  imageBuffer: CVImageBuffer?,
+                  presentationTimeStamp: CMTime,
+                  presentationDuration: CMTime) {
+        guard let imageBuffer = imageBuffer else {
+            print("no decoded imageBuffer")
+            return
+        }
+
+        var formatDescriptionOut: CMVideoFormatDescription?
+        let res1 = CMVideoFormatDescriptionCreateForImageBuffer(allocator: kCFAllocatorDefault,
+                                                                imageBuffer: imageBuffer,
+                                                                formatDescriptionOut: &formatDescriptionOut
+        )
+        guard let formatDescription = formatDescriptionOut,
+              res1 == noErr else {
+                  print("failed to create formatDescription: \(res1)")
+                  return
+              }
+
+        var sampleTiming = CMSampleTimingInfo(duration: presentationDuration,
+                                              presentationTimeStamp: presentationTimeStamp,
+                                              decodeTimeStamp: .invalid)
+
+        var sampleBuffer: CMSampleBuffer?
+        let res2 = CMSampleBufferCreateForImageBuffer(allocator: kCFAllocatorDefault,
+                                                      imageBuffer: imageBuffer,
+                                                      dataReady: true,
+                                                      makeDataReadyCallback: nil,
+                                                      refcon: nil,
+                                                      formatDescription: formatDescription,
+                                                      sampleTiming: &sampleTiming,
+                                                      sampleBufferOut: &sampleBuffer)
+        guard let res = sampleBuffer,
+              res2 == noErr else {
+                  print("failed to create CMSampleBuffer: \(res2)")
+                  return
+              }
+        decodedSubject.send(res)
+    }
+
     func decode(data: CMSampleBuffer) {
-        decodedSubject.send(data)
+        if session == nil {
+            setup()
+        }
+        guard let session = session else { return }
+        print("decode frame")
+        let flags: VTDecodeFrameFlags = []
+        var infoFlagsOut: VTDecodeInfoFlags = []
+        let res = VTDecompressionSessionDecodeFrame(session,
+                                                    sampleBuffer: data,
+                                                    flags: flags,
+                                                    frameRefcon: nil,
+                                                    infoFlagsOut: &infoFlagsOut)
+        if res != noErr {
+            print("faield decode frame: \(res)")
+        }
     }
 }
