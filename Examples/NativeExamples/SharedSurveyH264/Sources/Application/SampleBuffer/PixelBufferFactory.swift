ba169e00c5c9ea3acaee5ac2a96d8fbf71c2b529
fuziki <hymne.shannon.projects@gmail.com>
2022-01-04 00:54:45 +0900

    add CdeclH264Streamer

---
+++ Examples/NativeExamples/SharedSurveyH264/Sources/Application/SampleBuffer/PixelBufferFactory.swift
@@ -0,0 +1,82 @@
+//
+//  SampleBufferVideoFactory.swift
+//  NativeExamples
+//
+//  Created by fuziki on 2021/12/26.
+//
+
+import CoreImage
+import CoreMedia
+import Foundation
+
+class PixelBufferFactory {
+    let context = CIContext()
+    var width: Int!
+    var height: Int!
+    var pixelBuffer: CVPixelBuffer?
+    var formatDescription: CMVideoFormatDescription?
+    init(width: Int, height: Int) {
+        makePixelBuffer(width: width, height: height)
+    }
+
+    private func makePixelBuffer(width: Int, height: Int) {
+        self.width = width
+        self.height = height
+#if !os(macOS)
+        let options: [String: Any]? = [ kCVPixelBufferIOSurfacePropertiesKey: [:] ] as [String: Any]
+#else
+        let options: [String: Any]? = nil
+#endif
+        let status1 = CVPixelBufferCreate(nil,
+                                          width,
+                                          height,
+                                          kCVPixelFormatType_32BGRA,
+                                          options as CFDictionary?,
+                                          &pixelBuffer)
+        guard status1 == noErr, let buff = pixelBuffer else {
+            return
+        }
+        let status2 = CMVideoFormatDescriptionCreateForImageBuffer(allocator: kCFAllocatorDefault,
+                                                                   imageBuffer: buff,
+                                                                   formatDescriptionOut: &formatDescription)
+        guard status2 == noErr else {
+            return
+        }
+    }
+
+    func make(size: CGSize, time: CMTime, render: (CIContext, CVPixelBuffer) -> Void) -> CMSampleBuffer? {
+        if width != Int(size.width) || height != Int(size.height) {
+            makePixelBuffer(width: Int(size.width), height: Int(size.height))
+        }
+        guard let buff = pixelBuffer,
+              let desc = formatDescription else {
+            return nil
+        }
+        CVPixelBufferLockBaseAddress(buff, CVPixelBufferLockFlags(rawValue: 0))
+        render(context, buff)
+        var tmp: CMSampleBuffer?
+        var sampleTiming = CMSampleTimingInfo()
+        sampleTiming.presentationTimeStamp = time
+        _ = CMSampleBufferCreateForImageBuffer(allocator: kCFAllocatorDefault,
+                                               imageBuffer: buff,
+                                               dataReady: true,
+                                               makeDataReadyCallback: nil,
+                                               refcon: nil,
+                                               formatDescription: desc,
+                                               sampleTiming: &sampleTiming,
+                                               sampleBufferOut: &tmp)
+        CVPixelBufferUnlockBaseAddress(buff, CVPixelBufferLockFlags(rawValue: 0))
+        return tmp
+    }
+    
+    func make(size: CGSize, render: (CIContext, CVPixelBuffer) -> Void) -> CVPixelBuffer? {
+        if width != Int(size.width) || height != Int(size.height) {
+            makePixelBuffer(width: Int(size.width), height: Int(size.height))
+        }
+        guard let buff = pixelBuffer else {
+            return nil
+        }
+        render(context, buff)
+        return buff
+    }
+}
---
+++ Sources/UnityVideoCreator/H264Streamer/Core/Encoder.swift
@@ -0,0 +1,102 @@
+//
+//  Encoder.swift
+//  NativeExamples
+//
+//  Created by fuziki on 2022/01/01.
+//
+
+import CoreMedia
+import CoreVideo
+import Combine
+import Foundation
+import VideoToolbox
+
+class Encoder {
+    private var session: VTCompressionSession?
+
+    public let encodedSampleBuffer: AnyPublisher<CMSampleBuffer, Never>
+    private let encodedSampleBufferSubject = PassthroughSubject<CMSampleBuffer, Never>()
+
+    private var cancellables: Set<AnyCancellable> = []
+    public init() {
+        encodedSampleBuffer = encodedSampleBufferSubject.eraseToAnyPublisher()
+    }
+
+    public func setup(width: Int32, height: Int32) {
+        var session: VTCompressionSession?
+        let encoderSpecification: CFDictionary? = nil
+        let imageBufferAttributes: CFDictionary? = nil
+        let res = VTCompressionSessionCreate(allocator: kCFAllocatorDefault,
+                                             width: width,
+                                             height: height,
+                                             codecType: kCMVideoCodecType_H264,
+                                             encoderSpecification: encoderSpecification,
+                                             imageBufferAttributes: imageBufferAttributes,
+                                             compressedDataAllocator: nil,
+                                             outputCallback: outputCallback,
+                                             refcon: Unmanaged.passUnretained(self).toOpaque(),
+                                             compressionSessionOut: &session)
+        if res != noErr {
+            print("failed create VTCompressionSession: \(res)")
+            return
+        }
+        VTSessionSetProperty(session!, key: kVTCompressionPropertyKey_AverageBitRate, value: 1_000_000 as CFTypeRef)
+        self.session = session
+    }
+
+    private let outputCallback: VTCompressionOutputCallback = { (outputCallbackRefCon: UnsafeMutableRawPointer?,
+                                                                 sourceFrameRefCon: UnsafeMutableRawPointer?,
+                                                                 status: OSStatus,
+                                                                 infoFlags: VTEncodeInfoFlags,
+                                                                 sampleBuffer: CMSampleBuffer?) in
+        guard let outputCallbackRefCon = outputCallbackRefCon else { return }
+        let refcon = Unmanaged<Encoder>.fromOpaque(outputCallbackRefCon).takeUnretainedValue()
+        refcon.callback(outputCallbackRefCon: outputCallbackRefCon,
+                        sourceFrameRefCon: sourceFrameRefCon,
+                        status: status,
+                        infoFlags: infoFlags,
+                        sampleBuffer: sampleBuffer)
+    }
+
+    private func callback(outputCallbackRefCon: UnsafeMutableRawPointer?,
+                          sourceFrameRefCon: UnsafeMutableRawPointer?,
+                          status: OSStatus,
+                          infoFlags: VTEncodeInfoFlags,
+                          sampleBuffer: CMSampleBuffer?) {
+        let infoFlags = infoFlags == .asynchronous ? ".asynchronous" : ".frameDropped"
+        let size = (sampleBuffer?.dataBuffer?.dataLength).flatMap { Float($0) / 1_000 } ?? -1
+        print("encoded infoFlags: \(infoFlags), size: \(size)KB")
+        guard status == noErr else {
+            print("encode error: \(status)")
+            return
+        }
+        guard let sampleBuffer = sampleBuffer else {
+            print("encode error: no sampleBuffer")
+            return
+        }
+        encodedSampleBufferSubject.send(sampleBuffer)
+    }
+
+    public func encode(imageBuffer: CVImageBuffer, presentationTimeStamp: CMTime, duration: CMTime) {
+        if session == nil {
+            setup(width: Int32(CVPixelBufferGetWidth(imageBuffer)), height: Int32(CVPixelBufferGetHeight(imageBuffer)))
+        }
+        guard let session = session else { return }
+        let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer)
+        let height = CVPixelBufferGetHeight(imageBuffer)
+        let width = CVPixelBufferGetWidth(imageBuffer)
+        let mb = Float(bytesPerRow * height / 1_000) / 1_000
+        print("encode frame size: \(mb)MB, w: \(width), h: \(height), bpp: \(bytesPerRow / width)")
+        var infoFlagsOut: VTEncodeInfoFlags = []
+        let res = VTCompressionSessionEncodeFrame(session,
+                                                  imageBuffer: imageBuffer,
+                                                  presentationTimeStamp: presentationTimeStamp,
+                                                  duration: duration,
+                                                  frameProperties: nil,
+                                                  sourceFrameRefcon: nil,
+                                                  infoFlagsOut: &infoFlagsOut)
+        if res != noErr {
+            print("faield encode frame: \(res)")
+        }
+    }
+}
