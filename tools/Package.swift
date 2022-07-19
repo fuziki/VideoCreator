db75514661f0e7c253a689f9af510a70d43b33a5
fuziki <hymne.shannon.projects@gmail.com>
2022-07-19 11:59:43 +0900

    update tools

--- tools/Package.swift
+++ tools/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,12 +8,12 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/realm/SwiftLint", from: "0.43.1")
+        .package(url: "https://github.com/realm/SwiftLint", from: "0.47.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .executableTarget(
             name: "tools",
             dependencies: []),
         .testTarget(
