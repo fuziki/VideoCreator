bd3d05e5af645ab3ac984fe6e98b2188a6f13477
fuziki <33706353+fuziki@users.noreply.github.com>
2022-07-21 22:42:51 +0900

    Update Package.swift

--- Examples/Package.swift
+++ Examples/Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "dummy",
+    name: "empty",
     products: [],
     targets: []
 )
